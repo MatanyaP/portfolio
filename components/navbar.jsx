7e42d0d301b83d6178cecb43e04ccacd19d07415
pmatan <pmatan@cambium.co.il>
2022-03-27 14:28:40 +0300

    .

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     <nav className="flex items-center flex-wrap bg-white-200 p-3">
         <LinkTemplate href="/">
             <a className="inline-flex items-center p-2 mr-4 ">
-                {/* <Image src={`${prefix}/images/logo.png`} width={'50%'} height={'50%'} className={'object-contain'} alt={'logo'} /> */}
+                <Image src={`${prefix}/images/logo.png`} width={'50%'} height={'50%'} className={'object-contain'} alt={'logo'} />
             </a>
         </LinkTemplate>
       <button
