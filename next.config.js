603c7631af946bdc39d7ddb4aa5337e6294f7783
pmatan <pmatan@cambium.co.il>
2022-03-27 10:16:04 +0300

    .

--- next.config.js
+++ next.config.js
@@ -2,7 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  exportPathMap: async function() {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/blog': { page: '/blog' },
+      '/contact': { page: '/contact' },
+      '/projects': { page: '/projects' },
+      '/resume': { page: '/resume' },
+    }
+  },
+  
 }
 
 module.exports = nextConfig
