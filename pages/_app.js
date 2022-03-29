05c5774b816382752130a4fe66cc2c372324252f
pmatan <pmatan@cambium.co.il>
2022-03-29 17:57:45 +0300

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
 import Head from 'next/head'
+import { prefix } from '../utils/prefix'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <title>Matan Peretz Portfolio</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href={`${prefix}/fav.ico`} />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet"/>
